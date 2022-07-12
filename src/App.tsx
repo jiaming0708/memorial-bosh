7cb1d39c53453c2b63ea96f90d57c442725743e7
Jimmy <jiaming0708@gmail.com>
2022-07-12 21:08:45 +0800

    remove useless variable

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as OvalIcon } from './images/oval.svg';
 import Card from './components/Card';
 
 function App() {
-  const { data, loading, error } = useGoogleSheets({
+  const { data, loading } = useGoogleSheets({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
     sheetsNames: ['表單回應 1']
