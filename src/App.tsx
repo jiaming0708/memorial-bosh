a88b01306d60a47822837009e29e18ab1008b921
Jimmy <jiaming0708@gmail.com>
2022-07-12 21:44:44 +0800

    error handle

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as OvalIcon } from './images/oval.svg';
 import Card from './components/Card';
 
 function App() {
-  const { data, loading } = useGoogleSheets({
+  const { data, loading, error } = useGoogleSheets({
     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
     sheetsNames: ['表單回應 1']
@@ -13,6 +13,10 @@ function App() {
 
   if (loading) {
     return <OvalIcon />;
+  }
+
+  if (error) {
+    return <div>oh no, some error occurs!</div>
   }
 
   const [{ data: rows }] = data;
