fa27c5eab625870aa520ec02848e0e9f2890a88c
Jimmy <jiaming0708@gmail.com>
2022-07-12 20:32:28 +0800

    fetch data and loading icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import useGoogleSheets from 'use-google-sheets';
+
 import './App.scss';
+import { ReactComponent as OvalIcon } from './images/oval.svg';
 
 function App() {
+  const { data, loading, error } = useGoogleSheets({
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
+    sheetsNames: ['表單回應 1']
+  });
+
+  if (loading) {
+    return <OvalIcon />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
