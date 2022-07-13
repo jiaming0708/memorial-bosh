c90d0546a3002a13f3eb7a93586e3df4347aa2e2
Jimmy Ho <jiaming0708@gmail.com>
2022-07-13 14:06:37 +0800

    change image and adjust card UI

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,13 @@
 import { RowData } from "../interface/row-data";
 import styles from './Card.module.scss';
-import logo from '../images/logo.png';
 
 function Card({ who, words }: RowData) {
   return (
     <div className={styles.root}>
-      <div className={styles.header}>
-        <img className={styles.image} src={logo} alt="logo" />
-        <span className={styles.who}>{who}</span>
-      </div>
       <p className={styles.words}>
         {words}
       </p>
+      <div className={styles.who}>{who}</div>
     </div>
   );
 }
